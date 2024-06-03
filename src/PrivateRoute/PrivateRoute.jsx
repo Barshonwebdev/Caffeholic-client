2a81017050fda302f55677e3a60c3e1ec06d39c2
Barshon <barshonweb@gmail.com>
2024-06-03 12:16:39 +0600

    private route made

--- src/PrivateRoute/PrivateRoute.jsx
+++ src/PrivateRoute/PrivateRoute.jsx
@@ -1,10 +1,19 @@
+import React from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import useAuth from '../Hooks/useAuth';
 
-const PrivateRoute = () => {
-    return (
-        <div>
-            
+const PrivateRoute = ({children}) => {
+    const {user,loading}=useAuth();
+    const location=useLocation();
+    if(loading){
+       return <div>
+            <p>loading....</p>
         </div>
-    );
+    }
+    if(user){
+    return children;
+    }
+   return <Navigate to={'/login'} state={{from:location}} replace/>
 };
 
 export default PrivateRoute;
