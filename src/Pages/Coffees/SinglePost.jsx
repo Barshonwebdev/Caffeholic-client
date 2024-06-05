19e238dc8ad35377600fd584f4229377bd5c7fd3
Barshon <barshonweb@gmail.com>
2024-06-05 08:34:43 +0600

    each post design complete responsive

--- src/Pages/Coffees/SinglePost.jsx
+++ src/Pages/Coffees/SinglePost.jsx
@@ -17,18 +17,18 @@ const SinglePost = ({ post }) => {
     <div>
       <div className="card w-full text-amber-900">
         <figure className="px-5 pt-5">
-          <img src={photoURL} alt="Shoes" className="rounded-xl" />
+          <img src={photoURL} alt="Shoes" className="rounded-xl lg:w-1/2 md:w-2/3" />
         </figure>
         <div className=" items-center p-5 ">
-          <p className=" title-font text-3xl">Posted by: {poster}</p>
+          <p className=" title-font text-xl md:text-3xl">Posted by: {poster}</p>
           <hr className="mb-5 border-2 border-amber-900" />
           <h2 className="card-title title-font text-5xl">{coffee_name}</h2>
-          <p className="my-2 title-font text-3xl">{description}</p>
-          <h2 className="">Origin: {origin}</h2>
-          <p>Color: {color}</p> 
-          <p>Quick Recipe: {short_recipe}</p>
-          <p className="my-2">Type: {type}</p>
-          <p className="my-2">Personal experience: {personal_experience}</p>
+          <p className="my-2 title-font text-xl md:text-3xl">{description}</p>
+          <h2 className=""><span className="font-bold">Origin:</span> {origin}</h2>
+          <p><span className="font-bold">Color:</span> {color}</p> 
+          <p><span className="font-bold">Quick Recipe:</span> {short_recipe}</p>
+          <p className="my-2"><span className="font-bold">Type:</span> {type}</p>
+          <p className="my-2"><span className="font-bold">Personal Experience:</span> {personal_experience}</p>
         </div>
       </div>
     </div>
