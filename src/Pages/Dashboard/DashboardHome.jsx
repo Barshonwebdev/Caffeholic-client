23b310ae234bbade926136b736cc40163cd54d77
Barshon <barshonweb@gmail.com>
2024-06-05 18:32:21 +0600

    dashboard stats

--- src/Pages/Dashboard/DashboardHome.jsx
+++ src/Pages/Dashboard/DashboardHome.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Drawer from "../../components/DashboardComponents/Drawer";
 import useAuth from "../../Hooks/useAuth";
 import headerImg from '../../assets/coffee.png'
 
 const DashboardHome = () => {
   const {user}=useAuth();
+  const [thisUser,setThisUser]=useState({});
+  useEffect(()=>{
+    fetch(`https://caffeholic-server.vercel.app/user/${user?.email}`)
+    .then((res)=>res.json())
+    .then(data=>setThisUser(data))
+  },[user?.email])
+  const [stats,setStats]=useState({});
+  useEffect(()=>{
+    fetch(`https://caffeholic-server.vercel.app/stats`)
+    .then((res)=>res.json())
+    .then(data=>setStats(data))
+  },[])
   return (
    <div className="">
      <Drawer/>
@@ -15,11 +27,11 @@ const DashboardHome = () => {
      <div className="my-5 md:my-0 md:mx-10  card  items-center rounded border-amber-900   lg:w-96 shadow-2xl  shadow-amber-900 ">
      <h1 className="text-center text-3xl font-bold text-amber-900 title-font my-1">Profile</h1>
         <figure className="px-5 pt-2">
-          <img src={user?.photoURL} alt="Shoes" className="rounded-lg" />
+          <img src={thisUser?.photo} alt="Shoes" className="rounded-lg" />
         </figure>
         <div className=" items-center p-5 ">
-        <h1 className="text-center text-2xl font-bold text-amber-900 title-font my-1">Username: {user.displayName}</h1>
-        <h1 className="text-center text-2xl font-bold text-amber-900 title-font my-1">Email: {user.email}</h1>
+        <h1 className="text-center text-2xl font-bold text-amber-900 title-font my-1">Username: {thisUser.name}</h1>
+        <h1 className="text-center text-2xl font-bold text-amber-900 title-font my-1">Email: {thisUser.email}</h1>
         <div className="flex justify-center mt-4">
         <button className="btn btn-sm text-white bg-amber-900 hover:bg-amber-950">Edit Profile</button>
         </div>
@@ -31,8 +43,8 @@ const DashboardHome = () => {
           <img src={headerImg}  className="rounded-lg w-16" />
         </figure>
         <div className=" items-center p-5 ">
-        <h1 className="text-center text-2xl font-bold text-amber-900 title-font my-1">Username: {user.displayName}</h1>
-        <h1 className="text-center text-2xl font-bold text-amber-900 title-font my-1">Email: {user.email}</h1>
+        <h1 className="text-center text-3xl font-bold text-amber-900 title-font my-1">Total posts made: {stats.posts}</h1>
+        <h1 className="text-center text-3xl font-bold text-amber-900 title-font my-1">Total users: {stats.users}</h1>
        
         </div>
       </div>
