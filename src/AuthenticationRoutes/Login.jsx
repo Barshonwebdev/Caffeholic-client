929a3c6d3b73b4e89ff149c1780760b870a4c847
Barshon <barshonweb@gmail.com>
2024-09-01 17:54:54 +0600

    space

--- src/AuthenticationRoutes/Login.jsx
+++ src/AuthenticationRoutes/Login.jsx
@@ -73,7 +73,7 @@ const Login = () => {
         .then((res)=>res.json())
         .then(data=>{
           localStorage.setItem('token',data?.token)
-          console.log(data)})
+          console.log(data)}) 
       }
       navigate(from);
       Swal.fire('user logged in!')
